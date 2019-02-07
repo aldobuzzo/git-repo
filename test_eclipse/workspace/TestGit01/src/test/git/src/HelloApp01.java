fd7b5f9c77be8a5d182a7ccb4f6ade6cadb58a5f
aldobuzzo <aldo_buzzo@it.ibm.com>
2019-02-07 11:53:51 +0100

    HelloApp01 Step B

--- test_eclipse/workspace/TestGit01/src/test/git/src/HelloApp01.java
+++ test_eclipse/workspace/TestGit01/src/test/git/src/HelloApp01.java
@@ -3,7 +3,7 @@ package test.git.src;
 public class HelloApp01 {
 
 	public HelloApp01() {
-		// TODO Auto-generated constructor stub
+		System.out.println("HelloApp01 was invoked");
 	}
 	public void sayHello(){
 		System.out.println("App01 says Hello!");
