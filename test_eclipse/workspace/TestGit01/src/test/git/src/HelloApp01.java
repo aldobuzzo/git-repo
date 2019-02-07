788416751f480adac573ce70461b837726bf8c65
aldobuzzo <aldo_buzzo@it.ibm.com>
2019-02-07 11:16:39 +0100

    all

---
+++ test_eclipse/workspace/TestGit01/src/test/git/src/HelloApp01.java
@@ -0,0 +1,11 @@
+package test.git.src;
+
+public class HelloApp01 {
+
+	public HelloApp01() {
+		// TODO Auto-generated constructor stub
+	}
+	public void sayHello(){
+		System.out.println("App01 says Hello!");
+	}
+}
