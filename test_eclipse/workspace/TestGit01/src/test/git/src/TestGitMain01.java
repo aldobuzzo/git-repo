1c4c02ca807682d59a95591f5041469c989e8e7a
aldobuzzo <aldo_buzzo@it.ibm.com>
2019-02-07 11:50:06 +0100

    TestGitMain01 1

--- test_eclipse/workspace/TestGit01/src/test/git/src/TestGitMain01.java
+++ test_eclipse/workspace/TestGit01/src/test/git/src/TestGitMain01.java
@@ -4,6 +4,8 @@ public class TestGitMain01 {
 
 	public static void main(String[] args) {
 		System.out.println("Start Main");
+		SayHello01 sayHello = new SayHello01();
+		sayHello.sayHello();
 		System.out.println("End Main");
 	}
 
