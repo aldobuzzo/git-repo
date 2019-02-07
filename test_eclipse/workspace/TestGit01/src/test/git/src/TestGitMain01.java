e88e7613dcef8e4521550a5e04061112ebc6f98c
aldobuzzo <aldo_buzzo@it.ibm.com>
2019-02-07 12:10:33 +0100

    Generate Conflict B

--- test_eclipse/workspace/TestGit01/src/test/git/src/TestGitMain01.java
+++ test_eclipse/workspace/TestGit01/src/test/git/src/TestGitMain01.java
@@ -3,8 +3,8 @@ package test.git.src;
 public class TestGitMain01 {
 
 	public static void main(String[] args) {
-		System.out.println("Start Main");
-		System.out.println("End Main");
+		System.out.println("Start Main program");
+		System.out.println("End Main program");
 	}
 
 }
