317e1a9c6566d78684ada858725b7fa4a13c2590
aldobuzzo <aldo_buzzo@it.ibm.com>
2019-02-07 10:56:35 +0100

    Main

--- test_eclipse/workspace/TestGit01/src/test/git/src/TestGitMain01.java
+++ test_eclipse/workspace/TestGit01/src/test/git/src/TestGitMain01.java
@@ -3,7 +3,9 @@ package test.git.src;
 public class TestGitMain01 {
 
 	public static void main(String[] args) {
+		System.out.println("Start Main");
 		System.out.println("Hello git");
+		System.out.println("End Main");
 	}
 
 }
